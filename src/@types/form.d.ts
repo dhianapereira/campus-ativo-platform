cb113ca561cb15a24c323ddcc3916823d90a5578
dhianapereira <dhianaslv@outlook.com>
2024-02-09 00:29:32 -0300

    feat: Add input validations

---
+++ src/@types/form.d.ts
@@ -0,0 +1,4 @@
+import { loginFormSchema } from "@/validators/login-form";
+import { z } from "zod";
+
+type LoginFormData = z.infer<typeof loginFormSchema>
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,10 +1,8 @@
-import { Heading, LinkButton, Text, styled } from "@campusativo-ui/react";
+import { Heading, LinkButton, Text, TextInput, styled } from "@campusativo-ui/react";
 
 export const Container = styled('main', {
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
 
     maxWidth: 1440,
     margin: '$20 auto $4',
@@ -14,6 +12,7 @@ export const Container = styled('main', {
 export const IllustrationContainer = styled('div', {
     paddingRight: '$8',
     maxWidth: '100%',
+    marginTop: '$20',
 
     '@media(max-width: 820px)': {
         display: 'none',
@@ -46,14 +45,14 @@ export const Form = styled('form', {
         display: 'flex',
         flexDirection: 'column',
         gap: '$4',
-
-        [`> ${Text}`]: {
-            color: '$darkGray',
-        },
     },
 
     [`> ${LinkButton}`]: {
         alignSelf: 'end',
         marginBottom: '$4',
     },
+})
+
+export const FormError = styled(Text, {
+    color: '$red',
 })
