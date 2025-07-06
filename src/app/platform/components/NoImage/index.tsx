5d16e996cbffb49efc6ac9259906d489b86f21ed
Alex Custódio <alex.scustod@gmail.com>
2025-07-06 14:40:49 -0300

    fix: image optional handling

---
+++ src/app/platform/components/NoImage/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { ImageSquare } from 'phosphor-react'
+import {
+  NoImageContainer,
+  NoImageIcon,
+  NoImageTitle,
+  NoImageDescription,
+} from './styles'
+
+export interface NoImageProps {
+  title?: string
+  description?: string
+}
+
+export const NoImage = ({
+  title = 'Nenhuma imagem disponível',
+  description = 'Não há imagem para exibir neste momento.',
+}: NoImageProps) => {
+  return (
+    <NoImageContainer>
+      <NoImageIcon>
+        <ImageSquare size={32} weight="bold" alt="" />
+      </NoImageIcon>
+      <div>
+        <NoImageTitle>{title}</NoImageTitle>
+        <NoImageDescription>{description}</NoImageDescription>
+      </div>
+    </NoImageContainer>
+  )
+}
+
+NoImage.displayName = 'NoImage'
